53f5c25d6226597dc6b0cfd6eca9c01aa7acfec4
ayanared <ayanared.gmail.com>
2018-11-09 18:35:11 -0500

    passes second test

--- code_wars_practice/series_sum/series_sum.rb
+++ code_wars_practice/series_sum/series_sum.rb
@@ -1,3 +1,9 @@
 def series_sum(n)
-    sprintf('%.2f', n)
+    answer = 1
+    n = n-1
+    (0...n).each do |i|
+        answer = answer + 1/(1+3*n).to_f
+        puts answer
+    end
+    sprintf('%.2f', answer)
 end
