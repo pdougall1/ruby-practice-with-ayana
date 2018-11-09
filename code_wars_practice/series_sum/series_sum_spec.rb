e6cc3941d1942d51455d1a8264fae3d3ab73ab8a
ayanared <ayanared.gmail.com>
2018-11-09 17:18:24 -0500

    added second test to test 2

--- code_wars_practice/series_sum/series_sum_spec.rb
+++ code_wars_practice/series_sum/series_sum_spec.rb
@@ -1,9 +1,15 @@
 require_relative "series_sum"
 
 RSpec.describe "series_sum" do
-    context "when given an integer" do
-        it "returns the sum of a series" do
+    context "when given the integer, 1" do
+        it "returns the sum of a series, '1.00' " do
             expect(series_sum(1)).to eq("1.00")
+        end
+    end
+
+    context "when given the integer, 1" do
+        it "returns the sum of a series, '1.00' " do
+            expect(series_sum(2)).to eq("1.25")
         end
     end
 end
