db81a849a9ec66cb266f99a154424a9407d65613
ayanared <ayanared.gmail.com>
2018-11-10 12:02:47 -0500

    cleaned up code

--- code_wars_practice/find_outlier/find_outlier.rb
+++ code_wars_practice/find_outlier/find_outlier.rb
@@ -1,14 +1,11 @@
 def find_outlier(integers)
-    
     count_even = integers.select { |num| num.even? }.length
+
     if count_even >= 2 then
-        answer = integers.select {|i| i % 2 != 0 }[0]
-        puts answer.to_s
-
+        answer = integers.select {|i| i % 2 != 0 }.first
     else 
-        answer = integers.select {|i| i % 2 == 0}[0]
+        answer = integers.select {|i| i % 2 == 0}.first
     end
-
+    
     answer
-
 end
