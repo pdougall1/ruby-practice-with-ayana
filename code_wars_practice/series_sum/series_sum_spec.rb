ad6e0b07b17b85af2fc2022a4feb89a75f3d28b4
ayanared <ayanared.gmail.com>
2018-11-10 11:47:17 -0500

    fixed test 2 wording

--- code_wars_practice/series_sum/series_sum_spec.rb
+++ code_wars_practice/series_sum/series_sum_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe "series_sum" do
         end
     end
 
-    context "when given the integer, 1" do
-        it "returns the sum of a series, '1.00' " do
+    context "when given the integer, 2" do
+        it "returns the sum of a series, '1.25' " do
             expect(series_sum(2)).to eq("1.25")
         end
     end
